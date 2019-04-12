4dbfb3597716dbd0449f1fcda77462d3e6b894e9
Alexey Matveyev <AsMatveev@appliedtech.ru>
2019-04-12 17:37:34 +0500

    Хранение токенов в MongoDB

--- src/main/java/com/esb/oauthservice/config/TokenStoreConfig.java
+++ src/main/java/com/esb/oauthservice/config/TokenStoreConfig.java
@@ -1,15 +1,12 @@
 package com.esb.oauthservice.config;
 
-import com.esb.oauthservice.database.TokenDaoImpl;
 import com.esb.oauthservice.datasource.DataSourceManager;
+import com.esb.oauthservice.mongo.MongoTokenStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import javax.sql.DataSource;
 
 /**
  * TokenStoreConfig.java
@@ -24,15 +21,10 @@ public class TokenStoreConfig
     private DataSourceManager dataSourceManager;
 
 
-    public DataSource jdbcAuthDataSource()
-    {
-        return dataSourceManager.getDataSource(TokenDaoImpl.TOKEN_DB);
-    }
-
     @Bean
     public TokenStore tokenStore()
     {
-        return new JdbcTokenStore(jdbcAuthDataSource());
+        return new MongoTokenStore();
     }
 
     @Bean
