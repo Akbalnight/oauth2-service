ee35fdd572eed631c5e4018d539be93333867ee9
Alexey Matveyev <AsMatveev@appliedtech.ru>
2019-07-25 18:31:17 +0500

    ASSD-88. Подключил логгер

---
+++ src/main/java/com/esb/oauthservice/logger/LoggerConfig.java
@@ -0,0 +1,15 @@
+package com.esb.oauthservice.logger;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class LoggerConfig implements WebMvcConfigurer
+{
+    @Override
+    public void addInterceptors(InterceptorRegistry registry)
+    {
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+
+}
